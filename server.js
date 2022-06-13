bfc3520cf051a32caa3f30b9069fc7cb04e17103
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-06-13 16:21:57 -0500

    added images and some styling

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ const server = http.createServer((req, res) => {
 
         let random = Math.ceil(Math.random()*5)
 
-        let compThrow = random === 1 ? 'rock' : random === 2 ? 'paper' : random === 3 ? 'scissors' : random === 4 ? 'lizard' : 'spock'
+        let compThrow = random === 1 ? 'Rock' : random === 2 ? 'Paper' : random === 3 ? 'Scissors' : random === 4 ? 'Lizard' : 'Spock'
 
       if(userInput === 'rock' || userInput ==='paper' || userInput ==='scissors' || userInput ==='lizard' || userInput ==='spock'){
         res.writeHead(200, {'Content-Type': 'application/json'})
         function compare(comp, user){
-            if(comp === user){
+            if(comp.toLowerCase() === user){
                 return 'It is a tie!'
             }else if(
-                (comp === 'rock' && (user === 'scissors' || user === 'lizard')) || 
-                (comp === 'paper' && (user === 'rock' || user ==='spock')) || 
-                (comp === 'scissors' && (user === 'paper' || user ==='lizard')) || 
-                (comp === 'lizard' && (user === 'paper' || user ==='spock')) || 
-                (comp === 'spock' && (user === 'rock' || user ==='scissors'))){
+                (comp.toLowerCase() === 'rock' && (user === 'scissors' || user === 'lizard')) || 
+                (comp.toLowerCase() === 'paper' && (user === 'rock' || user ==='spock')) || 
+                (comp.toLowerCase() === 'scissors' && (user === 'paper' || user ==='lizard')) || 
+                (comp.toLowerCase() === 'lizard' && (user === 'paper' || user ==='spock')) || 
+                (comp.toLowerCase() === 'spock' && (user === 'rock' || user ==='scissors'))){
                 return 'Computer WINS!'
             }else{
                 return 'YOU WIN!'
@@ -54,17 +54,23 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify(objToJson));
       }//student != leon
     }//student if
-  }else if (page == '/css/style.css'){
+  }else if (page === '/css/style.css'){
     fs.readFile('css/style.css', function(err, data) {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
+  }else if (page === '/js/main.js'){
     fs.readFile('js/main.js', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data);
       res.end();
-    });
+    })
+  }else if (page === '/rpsls.png'){
+        fs.readFile('rpsls.png', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'img'});
+          res.write(data);
+          res.end();
+        });
   }else{
     figlet('404!!', function(err, data) {
       if (err) {
